44016b3f933078fc4013d47d549280602a936aa1
Jamie Brandon <jamie@scattered-thoughts.net>
2015-11-29 18:12:08 +0000

    Matchbox it up

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170" :classifier "aot"
                   :exclusion [org.clojure/data.json]]
-                 [org.clojure/data.json "0.2.6" :classifier "aot"]]
+                 [org.clojure/data.json "0.2.6" :classifier "aot"]
+                 [matchbox "0.0.8-SNAPSHOT"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.1"]]
   :npm {:dependencies [[source-map-support "0.3.2"]]}
--- src/cc_practice/core.cljs
+++ src/cc_practice/core.cljs
@@ -1,9 +1,28 @@
 (ns cc-practice.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require [clojure.browser.repl :as repl]
+            [matchbox.core :as m]))
 
-;; (defonce conn
-;;   (repl/connect "http://localhost:9000/repl"))
+(defonce conn
+    (repl/connect "http://localhost:9000/repl"))
 
 (enable-console-print!)
 
 (println "Hello world!")
+
+(def root (m/connect "https://el-sistema.firebaseio.com"))
+
+(m/auth-anon root)
+
+(m/listen-children
+  root [:users :mike :friends]
+  (fn [[event-type data]] (prn event-type data)))
+
+(def mikes-friends (m/get-in root [:users :mike :friends]))
+(m/reset! mikes-friends [{:name "Kid A"} {:name "Kid B"}])
+(m/conj! mikes-friends {:name "Jean"})
+
+(m/deref
+  mikes-friends
+  (fn [key value]
+    (m/reset-in! root [:users :mike :num-friends]
+                 (count value))))
