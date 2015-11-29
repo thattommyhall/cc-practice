d7d518f399998a43602c238e7c525448209ada44
thattommyhall <thattommyhall@gmail.com>
2015-11-29 17:06:02 +0000

    init

---
+++ src/cc_practice/core.cljs
@@ -0,0 +1,9 @@
+(ns cc-practice.core
+  (:require [clojure.browser.repl :as repl]))
+
+;; (defonce conn
+;;   (repl/connect "http://localhost:9000/repl"))
+
+(enable-console-print!)
+
+(println "Hello world!")
